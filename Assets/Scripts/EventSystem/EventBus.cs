2dec384087a67ad5245cc4a8e9856bdaf15f6d39
Monkey2521 <91818669+Monkey2521@users.noreply.github.com>
2022-08-23 20:31:03 +0500

    Add event system

--- Assets/Scripts/EventSystem/EventBus.cs
+++ Assets/Scripts/EventSystem/EventBus.cs
@@ -3,4 +3,67 @@ using System.Collections.Generic;
 
 public static class EventBus
 {
+    private static Dictionary<Type, SubscriberList> _subscribers = new Dictionary<Type, SubscriberList>();
+
+    private static bool _onPublish;
+
+    public static void Subscribe(ISubscriber subscriber)
+    {
+        List<Type> interfaces = GetSubscriberInterfaces(subscriber.GetType());
+
+        foreach (Type interfaceType in interfaces)
+        {
+            if (!_subscribers.ContainsKey(interfaceType))
+            {
+                _subscribers[interfaceType] = new SubscriberList();
+            }
+
+            _subscribers[interfaceType].Add(subscriber);
+        }
+    }
+
+    public static void Unsubscribe(ISubscriber subscriber)
+    {
+        List<Type> interfaces = GetSubscriberInterfaces(subscriber.GetType());
+
+        foreach (Type interfaceType in interfaces)
+        {
+            if (!_subscribers.ContainsKey(interfaceType))
+            {
+                return;
+            }
+
+            _subscribers[interfaceType].Remove(subscriber, _onPublish);
+        }
+    }
+
+    private static List<Type> GetSubscriberInterfaces(Type subscriber)
+    {
+        List<Type> interfaces = new List<Type>();
+
+        foreach (Type type in subscriber.GetInterfaces())
+        {
+            if (typeof(ISubscriber).IsAssignableFrom(type) && type != typeof(ISubscriber))
+                interfaces.Add(type);
+        }
+
+        return interfaces;
+    }
+
+    public static void Publish<TSubscriber>(Action<TSubscriber> action) where TSubscriber : ISubscriber
+    {
+        Type type = typeof(TSubscriber);
+
+        if (!_subscribers.ContainsKey(type))
+        {
+            return;
+        }
+
+        _onPublish = true;
+
+        _subscribers[type].RaiseEvent(action);
+        _subscribers[type].Cleanup();
+
+        _onPublish = false;
+    }
 }
--- Assets/Scripts/EventSystem/SubscriberList.cs
+++ Assets/Scripts/EventSystem/SubscriberList.cs
@@ -3,5 +3,60 @@ using System.Collections.Generic;
 
 public sealed class SubscriberList
 {
-    
+    private List<ISubscriber> _subscribers;
+
+    private bool _needCleanup;
+
+    public SubscriberList()
+    {
+        _subscribers = new List<ISubscriber>();
+        _needCleanup = false;
+    }
+
+    public void Add(ISubscriber subscriber)
+    {
+        if (_subscribers.Contains(subscriber)) return;
+
+        _subscribers.Add(subscriber);
+    }
+
+    public bool Remove(ISubscriber subscriber, bool onPublish)
+    {
+        if (_subscribers.Contains(subscriber))
+        {
+            if (onPublish)
+            {
+                _needCleanup = true;
+                _subscribers[_subscribers.IndexOf(subscriber)] = null;
+                return true;
+            }
+            else
+            {
+                return _subscribers.Remove(subscriber);
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void RaiseEvent<TSubscriber>(Action<TSubscriber> action) where TSubscriber : ISubscriber
+    {
+        foreach (ISubscriber subscriber in _subscribers)
+        {
+            action.Invoke((TSubscriber)subscriber);
+        }
+    }
+
+    public void Cleanup()
+    {
+        if (_needCleanup)
+        {
+            _subscribers.RemoveAll(sub => sub == null);
+            _needCleanup = false;
+        }
+
+        else return;
+    }
 }
